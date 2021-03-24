e6ec3d3d620a57d82cef9dacc2f14550641b2b6b
linhuangwei <244020155@qq.com>
2021-03-24 14:14:26 +0800

    完成登录注册页面

---
+++ pages/ChangePassword.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div style="width:500px; margin:auto;">
+      <div style="margin-top:200px">
+        <el-steps :active="active" finish-status="success">
+          <el-step title="步骤 1"></el-step>
+          <el-step title="步骤 2"></el-step>
+          <el-step title="步骤 3"></el-step>
+        </el-steps>
+
+        <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChangePassword",
+  data() {
+    return {
+      active: 0
+    };
+  },
+
+  methods: {
+    next() {
+      if (this.active++ > 2) this.active = 0;
+    }
+  }
+};
+</script>
+
+<style></style>
